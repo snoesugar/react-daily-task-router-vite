68307a6639d23dc3e830df361b481dfdb4139fcf
happy <daisy831015@gmail.com>
2026-02-13 10:59:20 +0800

    增加useNavigate()

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,5 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
+
+const Logout = () => {
+  const navigate = useNavigate();
+  return (
+    <button 
+      type="button" 
+      className='btn btn-primary' 
+      onClick={() => { navigate('/login', { replace: true });  }}
+    >登出</button>
+  )
+}
+
 function Todo () {
-  return <p>這是 Todo 頁面</p>;
+  return (
+    <>
+      <p>這是 Todo 頁面</p>
+      <Logout />
+    </>
+  );
 }
 
 export default Todo
